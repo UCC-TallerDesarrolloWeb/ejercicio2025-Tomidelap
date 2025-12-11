f109130375303b41d203504d8ed88398942d9813
Tomidelap <2409737@ucc.edu.ar>
2025-12-11 12:18:33 -0300

    Se relizo el ejercicio de conversor de unidades

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,18 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+
+function cambiarUnidades (id, Valor){
+    if (isNaN(Valor)){
+        alert("se ingreso un valor invalido")
+        document.lasUnidades.unid_metro.value = "";
+    } else if (id=="metro") {
+        document.lasUnidades.unid_pulgada.value = 39.3701*Valor;
+        document.lasUnidades.unid_pie.value = 3.28084*Valor;
+        document.lasUnidades.unid_yarda.value = 1.09361*Valor; 
+
+    }
+
+
+
+}
