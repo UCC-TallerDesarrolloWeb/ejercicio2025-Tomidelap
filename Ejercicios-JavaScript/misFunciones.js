2fc59cd7a76c1ca17630e9eae773459294c44e45
Tomidelap <2409737@ucc.edu.ar>
2025-12-11 13:45:27 -0300

    Se realizo el ejercicio de ocultad/mostrar

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -1,8 +1,8 @@
 /**
- * Descripción
+ * Conversion de unidades de metros yardas, pies y pulgadas
  * @method Nombre de la función
- * @param Parámetro A
- * @param Parámetro B
+ * @param  {string} id - El id de los imputs de metros, yardas, pies y pulgadas
+ * @param {nummber} valor - tiene el valor de los imputs
  * @return Valor que retorna
  */
 
@@ -16,6 +16,39 @@ function cambiarUnidades (id, Valor){
         document.lasUnidades.unid_yarda.value = 1.09361*Valor; 
 
     }
+
+}
+
+function convertirGR (id) {
+    var grad, rad;
+    if(id=="grados") {
+        grad = document.getElementById(Elementld: "grados").value;
+        rad = (grad*Math.PI)/180;
+    }else if(id=="radianes"){
+        rad= document.getElementById(Elementld: "radianes").value;
+        grad=(rad*180)/Math.PI
+        
+    }
+     document.getElementById(Elementld: "grados").value = grad;
+     document.getElementById(Elementld: "radianes").value = rad;
+
+
+
+}
+
+function mostrar_ocultar (valorMO){
+
+
+        if(valorMo=="val mostrar"){
+            document.getElementById(elementid: "divMO").style.display = 'block';
+    } else if (valorMO== "val ocultar"){
+        document.getElementById(elementid: "divMO").style.display = 'none';
+
+    }
+
+
+
+
 
 
 
